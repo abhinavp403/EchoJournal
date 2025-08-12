52fced164996db9f9c7a775bac216a8d81f38175
abhinavp403 <abhinavp403@gmail.com>
2025-08-12 18:51:43 -0400

    Handling process death on create echo screen

--- app/src/main/java/dev/abhinav/echojournal/echos/presentation/create_echo/CreateEchoViewModel.kt
+++ app/src/main/java/dev/abhinav/echojournal/echos/presentation/create_echo/CreateEchoViewModel.kt
@@ -49,13 +49,31 @@ class CreateEchoViewModel(
     private val eventChannel = Channel<CreateEchoEvent>()
     val events = eventChannel.receiveAsFlow()
 
-    private val _state = MutableStateFlow(CreateEchoState(playbackTotalDuration = recordingDetails.duration))
+    private val restoredTopics = savedStateHandle.get<String>("topics")?.split(",")
+    private val _state = MutableStateFlow(CreateEchoState(
+        playbackTotalDuration = recordingDetails.duration,
+        titleText = savedStateHandle["titleText"] ?: "",
+        noteText = savedStateHandle["noteText"] ?: "",
+        topics = restoredTopics ?: emptyList(),
+        mood = savedStateHandle.get<String>("mood")?.let {
+            MoodUi.valueOf(it)
+        },
+        showMoodSelector = savedStateHandle.get<String>("mood") == null,
+        canSaveEcho = savedStateHandle.get<Boolean>("canSaveEcho") == true
+    ))
     val state = _state
         .onStart {
             if (!hasLoadedInitialData) {
                 observeAddTopicText()
                 hasLoadedInitialData = true
             }
+        }
+        .onEach { state ->
+            savedStateHandle["titleText"] = state.titleText
+            savedStateHandle["noteText"] = state.noteText
+            savedStateHandle["topics"] = state.topics.joinToString(",")
+            savedStateHandle["mood"] = state.mood?.name
+            savedStateHandle["canSaveEcho"] = state.canSaveEcho
         }
         .stateIn(
             scope = viewModelScope,
@@ -72,7 +90,7 @@ class CreateEchoViewModel(
             CreateEchoAction.OnDismissMoodSelector -> onDismissMoodSelector()
             CreateEchoAction.OnDismissTopicSuggestions -> onDismissTopicSuggestions()
             is CreateEchoAction.OnMoodClick -> onMoodClick(action.moodUi)
-            is CreateEchoAction.OnNoteTextChange -> {}
+            is CreateEchoAction.OnNoteTextChange -> onNoteTextChange(action.text)
             CreateEchoAction.OnPauseAudioClick -> audioPlayer.pause()
             CreateEchoAction.OnPlayAudioClick -> onPlayAudioClick()
             is CreateEchoAction.OnRemoveTopicClick -> onRemoveTopicClick(action.topic)
@@ -86,6 +104,12 @@ class CreateEchoViewModel(
             CreateEchoAction.OnNavigateBackClick,
             CreateEchoAction.OnGoBack -> onShowConfirmLeaveDialog()
         }
+    }
+
+    private fun onNoteTextChange(text: String) {
+        _state.update { it.copy(
+            noteText = text
+        ) }
     }
 
     private fun onPlayAudioClick() {
@@ -132,7 +156,8 @@ class CreateEchoViewModel(
 
     private fun onTitleTextChange(text: String) {
         _state.update { it.copy(
-            titleText = text
+            titleText = text,
+            canSaveEcho = text.isNotBlank() && it.mood != null
         ) }
     }
 
